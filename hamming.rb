b1e81bf38fa416bafc0d564810fec248665e0469
reyzaps <reyzaps@gmail.com>
2022-10-31 11:04:14 +0700

    Initialize TDD

---
+++ hamming.rb
@@ -0,0 +1,5 @@
+class Hamming
+  def self.compute(dna_1_string, dna_2_string)
+    0
+  end
+end
--- hamming_test.rb
+++ hamming_test.rb
@@ -3,4 +3,8 @@ require 'minitest/autorun'
 require_relative 'hamming'
 
 class HammingTest < Minitest::Test
+
+  def test_identical_strands
+    assert_equal 0, Hamming.compute('A', 'A')
+  end
 end
