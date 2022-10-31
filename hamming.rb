cdcbcda2a1ff60c5c67953897a93a9537bab553c
reyzaps <reyzaps@gmail.com>
2022-10-31 11:22:34 +0700

    correctly compute the hamming distance

--- hamming.rb
+++ hamming.rb
@@ -1,5 +1,5 @@
 class Hamming
   def self.compute(dna_1_string, dna_2_string)
-    0
+    dna_1_string.chars.zip(dna_2_string.chars).count{|l, r| l != r}
   end
 end
--- hamming_test.rb
+++ hamming_test.rb
@@ -7,4 +7,16 @@ class HammingTest < Minitest::Test
   def test_identical_strands
     assert_equal 0, Hamming.compute('A', 'A')
   end
+
+  def test_long_identical_strands
+    assert_equal 0, Hamming.compute("GGACTGA", "GGACTGA")
+  end
+
+  def test_complete_distance_in_single_nucleotide_strands
+    assert_equal 1, Hamming.compute("A", "G")
+  end
+
+  # def test_disallow_first_strand_longer
+  #   assert_raises(ArgumentError) { Haming.compute("AATG", "AAA") }
+  # end
 end
